4ab90a04d1d785e9da7b799ec8c8ad6d6c616ffd
Maarten Staa <maarten.staa@brightalley.nl>
2021-08-19 16:01:37 +0200

    Add countdown.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useMemo, useState } from "react";
+
+const SECONDS = 1;
+const MINUTES = 60;
+const HOURS = MINUTES * 60;
+const DAYS = HOURS * 24;
+const WEEKS = DAYS * 7;
+
+const FACTORS = [WEEKS, DAYS, HOURS, MINUTES, SECONDS];
+const FACTOR_LABELS = [
+  ["week", "weeks"],
+  ["day", "days"],
+  ["hour", "hours"],
+  ["minute", "minutes"],
+  ["second", "seconds"],
+];
+
+const MAX_FACTORS = 3;
+
+function timeDiff(now, target) {
+  const totalSeconds = Math.floor((target - now) / 1000);
+
+  const parts = [];
+  let remaining = totalSeconds;
+  // for (const factor of [WEEKS, DAYS, HOURS, MINUTES]) {
+  for (let i = 0; i < FACTORS.length; ++i) {
+    const factor = FACTORS[i];
+    const n = Math.floor(remaining / factor);
+    remaining = remaining % factor;
+
+    if (n === 0) {
+      continue;
+    }
+
+    parts.push(`${n} ${FACTOR_LABELS[i][n === 1 ? 0 : 1]}`);
+  }
+
+  console.log(parts, MAX_FACTORS, parts.length, parts.slice());
+  return (
+    parts.slice(0, MAX_FACTORS - 1).join(", ") +
+    (parts.length >= MAX_FACTORS
+      ? `${MAX_FACTORS > 2 ? "," : ""} and ${parts[MAX_FACTORS - 1]}`
+      : "")
+  );
+}
 
 function App() {
+  const targetDate = useMemo(() => new Date("2021-09-18 10:20:00"), []);
+  const [timeLeft, setTimeLeft] = useState(() =>
+    timeDiff(new Date(), targetDate)
+  );
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => setTimeLeft(timeDiff(new Date(), targetDate)),
+      1000
+    );
+
+    return () => clearInterval(interval);
+  }, [targetDate]);
+
   return (
     <div className="App">
+      <div className="App-background" />
+
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Vacation countdown</h1>
       </header>
+
+      <p className="App-countdown"> Only {timeLeft} left!</p>
     </div>
   );
 }
